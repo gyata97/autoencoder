195fdb5b543d31e14ffa3aa90b942fef6e096438
Nana Adjedu <nanakappanana@gmail.com>
2025-12-18 11:02:28 +0000

    init commit

---
+++ autoencoder.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+class Autoencoder(nn.Module):
+    def __init__(self):
+        super(Autoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(28 * 28, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+        )
